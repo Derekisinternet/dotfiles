3e8301a1405d8fd096551674f97d223a6376d65b
i862087 <i862087@15mbp-16302.dhcp.bve.sap.corp>
2017-12-12 15:12:13 -0800

    added logic to install script

--- install.sh
+++ install.sh
@@ -25,6 +25,10 @@ link $REPO/.vimrc ~
 # if for some reason there's no reference to .bashrc in .bash_profile, add a source" 
 if [ -f $HOME/.bash_profile ]; then
     grep -q .bashrc $HOME/.bash_profile && echo 'source $HOME/.bashrc' >> $HOME/.bash_profile
+else
+    # otherwise, create the file and append to it.
+    echo "creating .bash_profile . . ."
+    echo 'source $HOME/.bashrc' >> $HOME/.bash_profile
 fi
 
 source $HOME/.bashrc
